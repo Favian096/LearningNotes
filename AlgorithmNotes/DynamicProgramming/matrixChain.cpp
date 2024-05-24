040442b96a86a6523027b1911fa62a2b3d7358e4
Favian096 <98526804+Favian096@users.noreply.github.com>
2024-05-24 22:09:11 +0800

    docs: remove file

---
+++ AlgorithmNotes/DynamicProgramming/matrixChain.cpp
@@ -0,0 +1,19 @@
+/**
+* 矩阵连乘
+*/
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<std::vector<int>> matrix(2, std::vector<int>(3));
+
+    std::cout << matrix.size() << std::endl;
+    std::cout << matrix[0].size() << std::endl;
+
+    return 0;
+}
+
+
+
+
