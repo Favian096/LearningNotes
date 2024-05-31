d900b6116c1f411934d86beb2c5818360366e03f
Favian096 <98526804+Favian096@users.noreply.github.com>
2024-05-31 23:10:36 +0800

    fix: routine update

--- AlgorithmNotes/DynamicProgramming/matrixChain.cpp
+++ AlgorithmNotes/DynamicProgramming/matrixChain.cpp
@@ -3,110 +3,118 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <iomanip>
 
 using namespace std;
 
-
-int MatrixChain_GetOptimalValue(const int *p, int n, int *s)
-// p: (in) 矩阵的行数和列数
-// n: (in) 矩阵数量
-// s: (out) 子链最优断开位置 
-// 返回值：最优链的计算量 
-{
-	int *m=new int [(n+1)*(n+1)];
-	// 子链最少计算量 
-
-	for (int i=1; i<=n; i++)
-		m[i*(n+1)+i]=0;
-	for (int r=2; r<=n; r++) // r是子链的长度 
-	{
-		for (int i=1; i<=n-r+1; i++) // i是子链的起始位置
-		{
-			int j=i+r-1; // j是子链的终止位置
-			m[i*(n+1)+j]=m[(i+1)*(n+1)+j]+p[i-1]*p[i]*p[j];
-			s[i*(n+1)+j]=i;
-			
-			for (int k=i+1; k<j; k++) // k是子链的断开位置 
-			{
-				int t=m[i*(n+1)+k]+m[(k+1)*(n+1)+j]+p[i-1]*p[k]*p[j];
-				if (t<m[i*(n+1)+j])
-				{
-					m[i*(n+1)+j]=t;
-					s[i*(n+1)+j]=k;
-				}
-			}
-		} 
-	}
-	
-	int result=m[1*(n+1)+n];
-	delete [] m;
-	
-	return result;
+/**
+ * 回溯输出最优乘法顺序
+ * @param i
+ * @param j
+ * @param n
+ * @param disPosition
+ * @param t
+ */
+void MatrixChain_Traceback(int i, int j, int n, const vector<vector<int>> &disPosition, vector<int> &t) {
+    if (i == j) return;
+    int k = disPosition[i][j];
+    MatrixChain_Traceback(i, k, n, disPosition, t);
+    MatrixChain_Traceback(k + 1, j, n, disPosition, t);
+    if (i != k) {
+        t[i * 2]++;
+        t[k * 2 + 1]++;
+    }
+    if (k + 1 != j) {
+        t[k * 2 + 2]++;
+        t[j * 2 + 1]++;
+    }
 }
 
-void MatrixChain_Traceback(int i, int j, int n, const int *s, int *t)
-// i, j: (in)子链的起始和终止位置
-// n: (in) 矩阵数量
-// s: (in) 子链最优断开位置 
-// t: (out) 每个矩阵左侧'('的数量和右侧')'的数量
-{
-	if (i==j) return;
-	int k=s[i*(n+1)+j];
-	MatrixChain_Traceback(i, k, n, s, t);
-	MatrixChain_Traceback(k+1, j, n, s, t);
-	if (i!=k)
-	{
-		t[i+i]++;
-		t[k+k+1]++;
-	}
-	if (k+1!=j)
-	{
-		t[k+k+2]++;
-		t[j+j+1]++;
-	}
+/**
+ * 输出最优乘法顺序
+ * @param n
+ * @param t
+ */
+void MatrixChain_PrintResult(int n, const vector<int> &t) {
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < t[i * 2]; k++)
+            cout << '(';
+        cout << 'A' << i + 1;
+        for (int k = 0; k < t[i * 2 + 1]; k++)
+            cout << ')';
+    }
+    cout << endl;
 }
 
-void MatrixChain_PrintResult(int n, const int *t)
-// n: (in) 矩阵数量
-// t: (in) 每个矩阵左侧'('的数量和右侧')'的数量
-{
-	for (int i=1; i<=n; i++)
-	{
-		for (int k=0; k<t[i+i]; k++)
-			cout<<'(';
-		cout<<'A'<<i;
-		for (int k=0; k<t[i+i+1]; k++)
-			cout<<')';
-	}
-	cout<<endl;
-}
+/**
+ * 计算矩阵连乘的顺序 和 最小乘法次数
+ * @param matrices
+ */
+void matrixChain(std::vector<vector<int>> matrices) {
+    std::vector<vector<int>> multiplyNum(matrices.size(), vector<int>(matrices.size(), 0));
+    std::vector<vector<int>> disPosition(matrices.size(), vector<int>(matrices.size(), 0));
 
-void MatrixChain(const int *p, int n)
-// p: (in) 矩阵的行数和列数
-// n: (in) 矩阵数量
-{
-	int *s=new int [(n+1)*(n+1)]; // 子链最优断开位置
-	int *t=new int [n+n+2]; // 每个矩阵左侧'('的数量和右侧')'的数量
-	for (int i=2; i<=n+n+1; i++) t[i]=0;
-	
-	int result=MatrixChain_GetOptimalValue(p, n, s);
-	MatrixChain_Traceback(1, n, n, s, t);
-	cout<<"最少计算量="<<result<<endl;
-	cout<<"最优乘法顺序: ";
-	MatrixChain_PrintResult(n, t);
-	
-	delete [] s;
-	delete [] t;
-}
+//    for (int r = 2; r <= matrices.size(); ++r) {
+//        for (int begin = 0; begin <= matrices.size() - r; ++begin) {
+//            int end = begin + r - 1;
+//            multiplyNum[begin][end] = INT_MAX;
+//            for (int position = begin; position < end; ++position) {
+//                int multiplyTimes = multiplyNum[begin][position] + multiplyNum[position + 1][end] +
+//                                    matrices[begin][0] * matrices[position][1] * matrices[end][1];
+//                if (multiplyTimes < multiplyNum[begin][end]) {
+//                    multiplyNum[begin][end] = multiplyTimes;
+//                    disPosition[begin][end] = position;
+//                }
+//            }
+//        }
+//    }
 
-int main(int argc, char *argv[]) {
-	int n=6;
-	int p[]={30,35,15,5,10,20,25};
-	MatrixChain(p, n);
-	
-	cin.get();
-	return 0;
+    //从矩阵个数为两个开始枚举
+    for (int matrixNum = 1; matrixNum < matrices.size(); matrixNum++) {
+        //枚举开始位置的矩阵
+        for (int begin = 0; begin <= matrices.size() - matrixNum - 1; begin++) {
+            //结束位置
+            int end = begin + matrixNum;
+            multiplyNum[begin][end] = INT_MAX;
+            //计算乘法次数
+            for (int position = begin; position < end; position++) {
+                int calcMultiply =
+                        multiplyNum[begin][position] +  //左边的最小次数
+                        multiplyNum[position + 1][end] + //右边的最小次数
+                        matrices[begin][0] * matrices[position][1] * matrices[end][1];
+                if (calcMultiply < multiplyNum[begin][end]) {
+                    multiplyNum[begin][end] = calcMultiply;
+                    disPosition[begin][end] = position;
+                }
+            }
+        }
+    }
+
+    // 输出最小乘法次数
+    cout << "Minimal amount of computation = " << multiplyNum[0][matrices.size() - 1] << endl;
+    // 输出最优乘法顺序
+    cout << "Optimal multiplication order : ";
+    vector<int> t(matrices.size() * 2 + 2, 0);
+    MatrixChain_Traceback(0, matrices.size() - 1, matrices.size(), disPosition, t);
+    MatrixChain_PrintResult(matrices.size(), t);
+
 }
 
 
+int main() {
+    //初始化6个矩阵: 30*35 35*15 15*5 5*10 10*20 20*25
+    std::vector<vector<int>> matrices = {
+            {30, 35},
+            {35, 15},
+            {15, 5},
+            {5,  10},
+            {10, 20},
+            {20, 25}
+    };
 
+    matrixChain(matrices);
+
+    return 0;
+}
