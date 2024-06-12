4e88a364d5b0ec32cd0f9a6bd5c595d5e1ad49a3
Favian096 <98526804+Favian096@users.noreply.github.com>
2024-06-12 19:09:49 +0800

    fix: use cpp

--- AlgorithmNotes/DynamicProgramming/PolygonMaxima.cpp
+++ AlgorithmNotes/DynamicProgramming/PolygonMaxima.cpp
@@ -2,72 +2,108 @@
 * 多边形游戏
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <limits>
 
-#define N 100
-int n, v[N], m[N][N][2];
-char op[N];
+/**
+ * 计算左右子链最值
+ *
+ * @param delBord 被删除的边的位置
+ * @param size 子链大小
+ * @param off 断开位置
+ * @param minTmp 最小值tmp
+ * @param maxTmp 最大值tmp
+ * @param op 运算符数组
+ * @param minMaxVal 最大最小值计算数组
+ */
+void minMax(int delBord, int size, int off, int &minTmp, int &maxTmp,
+            int length, std::string &op,
+            std::vector<std::vector<std::vector<int>>> &minMaxVal) {
 
-void MinMax(int n, int i, int k, int j, int &minf, int &maxf) {//最大值和最小值
-    int e[5], r;
-    int a = m[i][k][0], b = m[i][k][1];//a是前段链最小值，b是最大值
-    r = (i + k - 1) % n + 1;//因为是循环可取余来做
-    int c = m[r][j - k][0], d = m[r][j - k][1];//c是后段链最小值，d是最大值
-    if (op[k - 1] == '+') {//加的话，最小相加即最小，最大相加即最大
-        minf = a + c;
-        maxf = b + d;
-    } else {//乘可能会出现负负得正的情况，所以需要考虑四种情况
-        e[1] = a * c;
-        e[2] = a * d;
-        e[3] = b * c;
-        e[4] = b * d;
-        minf = e[1];
-        maxf = e[1];
-        for (int t = 2; t <= 4; t++) {//遍历寻找最大最小
-            if (maxf < e[t]) {
-                maxf = e[t];
+    int tmp[5], r = (delBord + off - 1) % length + 1;
+    int a = minMaxVal[delBord][off][0], b = minMaxVal[delBord][off][1],
+            c = minMaxVal[r][size - off][0], d = minMaxVal[r][size - off][1];
+
+    if (op[off - 1] == '+') {
+        minTmp = a + c;
+        maxTmp = b + d;
+    } else {
+        tmp[1] = a * c;
+        tmp[2] = a * d;
+        tmp[3] = b * c;
+        tmp[4] = b * d;
+
+        minTmp = tmp[1];
+        maxTmp = tmp[1];
+
+        for (int index = 2; index < 5; index++) {
+            if (tmp[index] < minTmp) {
+                minTmp = tmp[index];
             }
-            if (minf > e[t]) {
-                minf = e[t];
+            if (tmp[index] > maxTmp) {
+                maxTmp = tmp[index];
+            }
+        }
+
+    }
+
+
+}
+
+/**
+ * 枚举多边形链路长度和断开位置
+ *
+ * @param arr 节点值
+ * @param op 此位置运算符
+ * @return 最大值
+ */
+std::pair<int, int> polygonMax(std::vector<int> &arr, std::string &op) {
+    int length = arr.size() - 1, minTmp = INT_MAX, maxTmp = INT_MIN;
+    std::vector<std::vector<std::vector<int>>>
+            minMaxVal(length + 1, std::vector<std::vector<int>>(length + 1, std::vector<int>(2, 0)));
+
+    for (int index = 1; index <= length; index++) {
+        minMaxVal[index][1][0] = arr[index];
+        minMaxVal[index][1][1] = arr[index];
+    }
+
+    //枚举链长度
+    for (int size = 2; size <= length; size++) {
+        //枚举被删除的边位置
+        for (int delBord = 1; delBord <= length; delBord++) {
+            //枚举断开位置
+            for (int off = 1; off <= size - 1; off++) {
+                minMax(delBord, size, off, minTmp, maxTmp, length, op, minMaxVal);
+
+                if (minTmp < minMaxVal[delBord][size][0]) {
+                    minMaxVal[delBord][size][0] = minTmp;
+                }
+                if (maxTmp > minMaxVal[delBord][size][1]) {
+                    minMaxVal[delBord][size][1] = maxTmp;
+                }
             }
         }
     }
+
+    std::pair<int, int> bestVal(1, minMaxVal[1][length][1]);
+    for (int index = 2; index <= length; index++) {
+        if (minMaxVal[index][length][1] > bestVal.second) {
+            bestVal.first = index - 1;
+            bestVal.second = minMaxVal[index][length][1];
+        }
+    }
+    return bestVal;
 }
 
 int main() {
-    int i, j, k, minf, maxf;
-    printf("请输入多边形顶点数：");
-    scanf("%d", &n);
+    std::vector<int> arr = {0, 5, -7, 4, 2};
+    std::string op("#++**");
+    std::pair<int, int> bestVal = polygonMax(arr, op);
 
-    for (i = 1; i <= n; i++) {
-        printf("请输入%d边的操作数和运算符：", i);
-        scanf("%d %c", &v[i], &op[i]);
-        m[i][1][0] = v[i];
-        m[i][1][1] = v[i];
-    }
+    std::cout << "best off position : " << bestVal.first << std::endl << "max value : " << bestVal.second;
 
-    for (j = 2; j <= n; j++) {
-        for (i = 1; i <= n; i++) {//删除的边
-            for (k = 1; k <= j - 1; k++) {//断链位置
-                MinMax(n, i, k, j, minf, maxf);
-                if (m[i][j][0] > minf) {
-                    m[i][j][0] = minf;
-                }
-                if (m[i][j][1] < maxf) {
-                    m[i][j][1] = maxf;
-                }
-            }
-        }
-    }
-    int temp = m[1][n][1], p = 1;
-    for (i = 2; i <= n; i++) {
-        if (m[i][n][1] > temp) {
-            temp = m[i][n][1];
-            p = i;
-        }
-    }
-    printf("首断链位置为：%d,最大得分为：%d\n", p, temp);
     return 0;
 }
