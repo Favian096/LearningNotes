323a663b03c68a1600ded53cb1ea58d25f5e9e07
Favian096 <98526804+Favian096@users.noreply.github.com>
2024-07-20 21:38:55 +0800

    feat: routine update

--- AlgorithmNotes/GreedyAlgorithm/GreedySelector.cpp
+++ AlgorithmNotes/GreedyAlgorithm/GreedySelector.cpp
@@ -1,6 +1,7 @@
 /**
 * 活动安排问题
 */
+
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -8,33 +9,56 @@
 
 using namespace std;
 
+typedef struct Activity {
+    int id;
+    int start;
+    int end;
+
+    Activity(int id, int start, int end) {
+        this->id = id;
+        this->start = start;
+        this->end = end;
+    }
+} Activity;
+
 /**
  * 贪心计算最大兼容活动个数
  *
- * @param s 活动开始时间集
- * @param f 活动结束时间集
+ * @param activities 活动集
  * @return 兼容活动个数和活动
  */
-pair<int, vector<int>> greedySelector(vector<int> s, vector<int> f) {
-    vector<int> activities = {1};
+pair<int, vector<int>> greedySelector(vector<Activity> activities) {
+//    已按结束时间排序
+    sort(activities.begin(), activities.end(), [](Activity a, Activity b) { return a.end < b.end; });
+    vector<int> activitiesId = {activities[0].id};
     int count = 1;
-    for (int i = 1, j = 0; i < s.size(); i++) {
-        if (s[i] >= f[j]) {
-            activities.push_back(i + 1);
-            j = i;
-            count++;
+
+    for (int i = 1, j = 0; i < activities.size(); i++) {
+        if (activities[i].start >= activities[j].end) {
+            activitiesId.push_back(activities[i].id);
+            j = i, count++;
         }
     }
-    return pair<int, vector<int>>(count, activities);
+    return pair<int, vector<int>>(count, activitiesId);
 }
 
 int main() {
     /**
      * 已按结束时间排序
      */
-    vector<vector<int>> S = {{1, 3, 0, 5, 3, 5, 6,  8,  8,  2,  12},
-                             {4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16}};
-    pair<int, vector<int>> result = greedySelector(S[0], S[1]);
+    vector<Activity> activities = {Activity(1, 0, 6),
+                                   Activity(2, 1, 4),
+                                   Activity(3, 2, 14),
+                                   Activity(4, 3, 5),
+                                   Activity(5, 3, 9),
+                                   Activity(6, 5, 7),
+                                   Activity(7, 5, 9),
+                                   Activity(8, 6, 10),
+                                   Activity(9, 8, 11),
+                                   Activity(10, 8, 12),
+                                   Activity(11, 12, 16)};
+
+    pair<int, vector<int>> result = greedySelector(activities);
     cout << "Maximum number of compatible activities: " << result.first << endl;
     cout << "Index of activities: ";
     for_each(result.second.begin(), result.second.end(), [](int index) { cout << index << " "; });
