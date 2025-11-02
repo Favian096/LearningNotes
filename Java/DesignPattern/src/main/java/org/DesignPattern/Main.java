3fdf03bc96d10870b3abb3b27278e8a8089f0aca
Favian096 <Favian096mail@gmail.com>
2025-11-02 23:32:00 +0800

    feat: design pattern

---
+++ Java/DesignPattern/src/main/java/org/DesignPattern/Main.java
@@ -0,0 +1,15 @@
+package org.DesignPattern;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println();
+    }
+}
